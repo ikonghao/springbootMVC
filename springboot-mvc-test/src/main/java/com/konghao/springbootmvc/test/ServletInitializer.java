8e75e465ada2549e0e2cc91f047051f174f48c99
konghao <konghao8013@163.com>
2017-12-24 16:23:36 +0800

    使用springboot创建模版引擎

---
+++ springboot-mvc-test/src/main/java/com/konghao/springbootmvc/test/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.konghao.springbootmvc.test;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootMvcTestApplication.class);
+	}
+
+}
---
+++ springboot-mvc-test/src/test/java/com/konghao/springbootmvc/test/SpringbootMvcTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konghao.springbootmvc.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMvcTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
